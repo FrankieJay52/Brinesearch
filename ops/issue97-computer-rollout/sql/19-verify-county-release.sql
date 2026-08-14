d979956f7c6df8c9ad8c787c389368e5677e23a0
FrankieJay52 <87081892+FrankieJay52@users.noreply.github.com>
2026-08-14 16:57:03 -0400

    Issue #97 verify ODOT-overlap release builder

--- ops/issue97-computer-rollout/sql/19-verify-county-release.sql
+++ ops/issue97-computer-rollout/sql/19-verify-county-release.sql
@@ -16,7 +16,7 @@ with latest as (
 select count(*)=1
   and pg_catalog.bool_and(private_verification.brinesearch_issue97_graph_build_release_current(id))
   and pg_catalog.bool_and(details->>'release_generation_key'='issue97-release-20260814-r1')
-  and pg_catalog.bool_and(details->>'release_builder_md5'='7abd11f432c3e7b475b10d0817f5e8fc')
+  and pg_catalog.bool_and(details->>'release_builder_md5'='793ed8985252b00d52f46da497484029')
   and pg_catalog.bool_and(details->>'release_supplemental_mapper_md5'='4dd8a572b153d795163cf38a41ea9d1f')
   and pg_catalog.bool_and(coalesce(details->>'release_source_content_digest','')~'^[0-9a-f]{32}$')
   as release_pass
